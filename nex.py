4e4ce9f4a1fc32d25f5d36fe2a645677aa842ccc
Nikhil George <nikhilgeo@gmail.com>
2016-04-21 09:11:18 +0530

    Nexpose API handler class

---
+++ nex.py
@@ -0,0 +1,11 @@
+class Nexpose:
+	"""All Nexpose Api handled here"""
+	def _init_(self):
+		# API v1.1 Login and get the session here
+	def siteManagement():
+		# API v1.1 SiteConfig- Provide the configuration of the site, including its associated assets.
+		# API v1.1 SiteSave- Save changes to a new or existing site.
+	def userManagement():
+		# API v1.1 UserSave- Create a new user account, or update the settings for an existing account. 
+	def _del_(self):
+		# API v1.1 Logout and close the session
