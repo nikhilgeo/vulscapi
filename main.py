2162907711a4baf6e2d822f6a02c5db4f8f887ce
Nikhil George <nikhilgeo@gmail.com>
2016-04-26 07:42:48 +0530

    read scnner info from xml file

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from defusedxml.ElementTree import parse
 
-tree = parse('access_request.xml')
-root = tree.getroot()
-for child in root.findall('user'):
-	name = child.find('name').text
-	print(name)
-	email = child.find('email').text
-	print(email)
-	
+#read access request from XML
+def readAccessReq():
+	tree = parse('access_request.xml')
+	root = tree.getroot()
+	for child in root.findall('user'):
+		name = child.find('name').text
+		print(name)
+		email = child.find('email').text
+		print(email)
+
+#read the scanner details
+def readScanner(scannerName):
+	tree = parse('scanner_details.xml')
+	root = tree.getroot()
+	scanner = root.find(scannerName)
+	#print(scanner)
+	print(scanner.find('host').text)
+	print(scanner.find('username').text)
+
+
+readScanner('nexpose')	
