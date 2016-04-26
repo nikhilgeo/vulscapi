99c5008f4f42a68295399f1363f0514114bf0469
Nikhil George <nikhilgeo@gmail.com>
2016-04-27 02:15:49 +0530

    Nexpose Login Sucess

--- nex.py
+++ nex.py
@@ -1,20 +1,31 @@
-from xml.etree.ElementTree import Element, SubElement, dump, ElementTree
+from xml.etree.ElementTree import Element, SubElement, ElementTree
 import requests
+from io import BytesIO
 #Have not used the diffusedXML library as the XML construction functions is only done here and they are not supported.
 class Nexpose:
 	'''All the Nexpose API are handled here'''
+	#self. - for all members that are common to the object without self scope is limited to the block
+	#self must the default mandatory parameter in all member functions	
+	#Caution !!: __init__ have two underscores on each side
+	
 	def __init__(self,scanner_info):
 		# API v1.1 Login and get the session here
-		root= Element('LoginRequest',attrib={'user-id':scanner_info['uname'],'password':scanner_info['passwd']})
+		xmlReq = Element('LoginRequest',attrib={'user-id':scanner_info['uname'],'password':scanner_info['passwd']})
 		self.nexpose_host = scanner_info['host']
-		dump(root)
-		makeRequest(root)
-		#LoginReqXML = ElementTree(root)
-		#LoginReqXML.write
+		xmlTree = ElementTree(xmlReq)
+		f=BytesIO()
+		xmlTree.write(f,encoding='utf-8',xml_declaration=True)# required so that xml declarations will come up in generated XML
+		self.loginReqXML=f.getvalue().decode("utf-8")# converts bytes to string
+		print(self.loginReqXML)
+		self.makeRequest()
+	def makeRequest(self):
+		headers = {'Content-Type': 'text/xml'}	
+		response=requests.post(self.nexpose_host+"/api/1.1/xml",data=self.loginReqXML,headers=headers,verify=False)
+		print(response.text)
+		
 	def siteManagement():
 		print("TestModule")
-	def makeRequest():
-		headers = {'Content-Type': 'text/xml'}	
+	
 	# API v1.1 SiteConfig- Provide the configuration of the site, including its associated assets.
 	# API v1.1 SiteSave- Save changes to a new or existing site.
 	#def userManagement():
