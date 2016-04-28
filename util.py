c9c6be2e8206c6ed7735e8a989b16f6a0400db6a
Nikhil George <nikhilgeo@gmail.com>
2016-04-28 23:57:04 +0530

    PrintUtil added for colored prinitng

--- util.py
+++ util.py
@@ -1,7 +1,17 @@
-class util:
-	"""Class to handle all the utility functions"""
-	def _init_(self):
-		# TBD
-	def sendRequest:
-		# routines to send POST requests over HTTPS.
-		
+class bcolors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	EXCEPTION = '\033[96m'	
+
+class PrintUtil:
+	def printSuccess(msg):
+		print(bcolors.OKGREEN + "[OK]"+msg + bcolors.ENDC)
+	def printError(msg):
+		print(bcolors.FAIL + "[ERROR]"+msg + bcolors.ENDC)
+	def printException(msg):
+		print(bcolors.FAIL +"[EXCEPTION]"+msg + bcolors.ENDC)
+
