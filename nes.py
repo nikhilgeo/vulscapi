de3c09a4fa45ac706359b172589ac9d1e4b14bed
nikhilgeorge <nikhilgeorge@vmware.com>
2016-05-16 18:34:44 +0530

    Log in, User creation done. Log Out TBD

--- nes.py
+++ nes.py
@@ -1,30 +1,77 @@
 import json
 import util
 from util import PrintUtil
+import requests
 
 
 class Nessus:
     """All Nessus related functions are handled here"""
 
     # Payload type JSON
-    # Login:Nessus.session: create
+    # Login:Nessus.session: create | Add it to your request using the following HTTP header: X-Cookie: token={token};
     # Create User: Nessus.users: create
     # Logout:Nessus.session: destroy
+
+
+    def makeRequest(self, url, payload, headers, method="POST"):
+        if method == "POST":
+            response = requests.post(url, data=payload, headers=headers, verify=False)
+            self.status_code = response.status_code
+            # print(response.text)
+        return response.content
 
     def __init__(self, scanner_info):
         try:
             # Login and get the session here
             self.nessus_host = scanner_info['host']
+            self.headers = {'Content-Type': 'application/json'}
             sessionreqURL = self.nessus_host + "/session"
             payload = {'username': scanner_info['uname'], 'password': scanner_info['passwd']}
-            response = util.makeRequest(sessionreqURL, json.dumps(payload), "json")
+            response = self.makeRequest(sessionreqURL, json.dumps(payload), self.headers)
             json_rep = json.loads(response.decode("utf-8"))  # convert to string then convert to json
-            #print(json_rep)
-            if 'error' in json_rep:
-                PrintUtil.printError("Login Failure: " + json_rep['error'])
-                return
-            if 'token' in json_rep:
-                PrintUtil.printSuccess("Logged in to Nessus Scanner")
+            # print(json_rep)
+            if self.status_code == 200:
                 self.session_token = json_rep['token']
+                self.headers.update({'X-Cookie': 'token='+self.session_token}) # session token added to HTTP header
+                print(self.headers)
+                PrintUtil.printSuccess("Logged in to Nessus Scanner")
+            if self.status_code == 400:
+                PrintUtil.printError("Login Failure: username format is not valid")
+            if self.status_code == 401:
+                PrintUtil.printError("Login Failure: username or password is invalid")
+            if self.status_code == 500:
+                PrintUtil.printError("Login Failure:  too many users are connected")
+        except Exception as e:
+            PrintUtil.printException(str(e))
+
+    def create_user(self, access_req):
+        try:
+            # Create User
+            create_user_URL = self.nessus_host + "/users"
+            usrLst = access_req['userList']
+            for user in usrLst:
+                userinfo = user.split(',')  # uname,name,email
+                pswd = userinfo[0] + '!vul5c4p1'
+                payload = {'username': userinfo[0], 'password': pswd, 'permissions': '32',
+                           'name': userinfo[1], 'email': userinfo[2], 'type': 'local'}
+                response = self.makeRequest(create_user_URL, json.dumps(payload), self.headers)
+                json_rep = json.loads(response.decode("utf-8"))
+                #print(json_rep)
+                if self.status_code == 200:
+                    PrintUtil.printSuccess("Created user: "+userinfo[1])
+                if self.status_code == 400:
+                    PrintUtil.printError("User creation Failure: Invalid field request")
+                if self.status_code == 403:
+                    PrintUtil.printError("User creation Failure: No permission to create a user")
+                if self.status_code == 409:
+                    PrintUtil.printError("User creation Failure: Duplicate username")
+
+        except Exception as e:
+            PrintUtil.printException(str(e))
+
+    def handleAccessReq(self, access_req):
+        try:
+            create_user_status = self.create_user(access_req)
+            #self.logoutOperation()
         except Exception as e:
             PrintUtil.printException(str(e))
