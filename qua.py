fe7f63e55328b0c7f91a46c973e967a6701d84f7
nikhilgeorge <nikhilgeorge@vmware.com>
2016-05-25 18:49:55 +0530

    HTTP basic auth implimented. Asset addition done

--- qua.py
+++ qua.py
@@ -1,6 +1,7 @@
 import requests
 from defusedxml.ElementTree import parse, fromstring
 from xml.etree.ElementTree import Element, SubElement, ElementTree
+from requests.auth import HTTPBasicAuth
 
 from util import PrintUtil
 
@@ -15,54 +16,48 @@ class Qualys:
 
     # Bloody Qualys, why can't u have a single API interface & both with different authentication methods !! F#@$
     # API v2 -- Asset Management, Session Based Login & Basic HTTP auth also
-        # Required Header Parameter  "X-Requested-With: <user description, like a user agent>"
-        # Login request returns a session ID in the Set-Cookie HTTP header
-        # Include the session ID in the cookie header for each request
+    # Required Header Parameter  "X-Requested-With: <user description, like a user agent>"
+    # Login request returns a session ID in the Set-Cookie HTTP header
+    # Include the session ID in the cookie header for each request
     # API v1 -- User Management, Basic HTTP Authentication
-        # Qualys account credentials are transmitted over HTTPS for each API call
+    # Qualys account credentials are transmitted over HTTPS for each API call
 
 
-    def login(self, scanner_info):
+    def __init__(self, scanner_info):
         self.qualys_host = scanner_info['host']
-        self.url = self.qualys_host + "/api/2.0/fo/session/"
-        self.cookie = ""
         self.headers = {'X-Requested-With': 'vulscapi'}
-        self.payload = {"action": "login", "username": scanner_info['uname'], "password": scanner_info['passwd']}
-        response = self.makeRequest()
-        if 'Set-Cookie' in response.headers:  # check if Set-Cookie is header is returned or not
-            # print(response.headers['Set-Cookie'])  # QualysSession=XXXXXXXXXXXXXXXXXXX; path=/api; secure
-            split1 = response.headers['Set-Cookie'].split(';')
-            QualysSession = split1[0].split('=')
-            self.cookie = {'QualysSession': QualysSession[1]}
-            # print(self.cookie)
-        print(response.text)
-        responseXML = response.content
-        tree = ElementTree(fromstring(responseXML))
-        root = tree.getroot()
-        login_response = root.find('RESPONSE')
-        login_status = login_response.find('TEXT').text
-        if login_status == "Logged in":
-            PrintUtil.printSuccess("Logged in to Qualys Scanner")
-            return True
-        else:
-            PrintUtil.printError("Login Failure: " + login_status)
-            return False
+        self.uname = scanner_info['uname']
+        self.passwd = scanner_info['passwd']
+
 
     def add_asset(self, access_req):
+        self.url = self.qualys_host + "/api/2.0/fo/asset/ip/?action=add"
+        self.url = self.url + "&ips=" +access_req['ip'] +"&enable_vm=1"
+        response_aasset_add = self.makeRequest()
+        print(response_aasset_add.content)
+        responseXML = response_aasset_add.content
+        tree = ElementTree(fromstring(responseXML))
+        root = tree.getroot()
+        asset_response = root.find('RESPONSE')
+        asset_status = asset_response.find('TEXT').text
+        if asset_status == "IPs successfully added to Vulnerability Management":
+            PrintUtil.printSuccess("Asset added to Qualys Scanner")
+            return True
+        else:
+            PrintUtil.printError("Asset adition Failure: " + asset_status)
+            return False
 
 
     def makeRequest(self):
-        response = requests.post(self.url, data=self.payload, cookies=self.cookie, headers=self.headers, verify=False)
+        response = requests.post(self.url, headers=self.headers, verify=False, auth=(self.uname, self.passwd))
         print(response.headers)
         return response
 
     def handleAccessReq(self, access_req, scanner_info):
         try:
-            login_success = self.login(scanner_info)
-            if login_success:
-                asset_adittion_success = self.add_asset(access_req)
-                if asset_adittion_success:
-                    # create_user_status = self.create_user(access_req, )
-                    # self.logout_user()
+            asset_adittion_success = self.add_asset(access_req)
+                # if asset_adittion_success:
+                # create_user_status = self.create_user(access_req, )
+                # self.logout_user()
         except Exception as e:
             PrintUtil.printException(str(e))
