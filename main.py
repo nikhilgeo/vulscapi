617c7b1870ab98bef23e0b463c4b7bbe9a9565ad
Nikhil George <nikhilgeo@gmail.com>
2016-06-07 18:22:37 +0530

    Command line parameters implemented

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import nex
 import nes
 import qua
 from util import Utilities
+import argparse
 
 
 # Global variables
 
 msg = ""
-# read access request from XML
 
+#---------------------Access Request Handler functions: Start----------------------------------------------------------
 def xml_error(msg):
     Utilities.printError(msg)
     raise SystemExit
 
 
-
 def readAccessReq():
+    # read access request from XML
     try:
         ip = ""
         usrlst = []
@@ -46,65 +47,78 @@ def readAccessReq():
     except Exception as e:
             Utilities.printException("Error with access_request.xml."+ str(e))
 
+
+def access_request_handler():
+    access_details = readAccessReq()  # Reading the Access Request
+    try:
+        tree = parse('scanner_details.xml')  # Reading the Scanner Details
+        root = tree.getroot()
+
+        # Access request handler for scanners
+        # Read Nessus scanner details
+        scanner = root.find('nessus')
+        execute_nessus = scanner.get('enabled')
+        if execute_nessus == '1':
+            # print(scanner)
+
+            if scanner.find('host').text is None or scanner.find('username').text is None or scanner.find('host').text is None:
+                xml_error("Nessus data missing in scanner_details.xml")
+            print("Nessus" + " host@:" + scanner.find('host').text)
+            # print(scanner.find('username').text)
+            usr_passwd = input("Please enter your password for " + " Nessus" + ": ")
+            nessus_details = {'uname': scanner.find('username').text, 'passwd': usr_passwd, 'host': scanner.find('host').text}
+            # Scanner task calls from here
+            Utilities.printLog("Executing Nessus tasks")
+            nessusObj = nes.Nessus(nessus_details)  # Create Nessus scanner class obj
+            msg = nessusObj.handleAccessReq(access_details, nessus_details)  # Login | Add User | Logout
+
+        # Read Nexpose scanner details
+        scanner = root.find('nexpose')
+        execute_nexpose = scanner.get('enabled')
+        if execute_nexpose == '1':
+            # print(scanner)
+            if scanner.find('host').text is None or scanner.find('username').text is None or scanner.find('host').text is None:
+                xml_error("Nexpose data missing in scanner_details.xml")
+            print("Nexpose" + " host@:" + scanner.find('host').text)
+            # print(scanner.find('username').text)
+            usr_passwd = input("Please enter your password for " + " Nexpose" + ": ")
+            nexpose_details = {'uname': scanner.find('username').text, 'passwd': usr_passwd, 'host': scanner.find('host').text}
+            # Scanner task calls from here
+            Utilities.printLog("Executing Nexpose tasks")
+            nexposeObj = nex.Nexpose(nexpose_details)  # Create Nexpose scanner class obj
+            msg += "\n"+nexposeObj.handleAccessReq(access_details, nexpose_details)  # Login | SaveSite | Add User | Logout
+
+        # Read Qualys scanner details
+        scanner = root.find('qualys')
+        execute_qualys = scanner.get('enabled')
+        if execute_qualys == '1':
+            # print(scanner)
+            if scanner.find('host').text is None or scanner.find('username').text is None or scanner.find('host').text is None:
+                xml_error("Qualys data missing in scanner_details.xml")
+            print("Qualys" + " host@:" + scanner.find('host').text)
+            # print(scanner.find('username').text)
+            usr_passwd = input("Please enter your password for " + " Qualys" + ": ")
+            qualys_details = {'uname': scanner.find('username').text, 'passwd': usr_passwd, 'host': scanner.find('host').text}
+            # Scanner task calls from here
+            Utilities.printLog("Executing Qualys tasks")
+            qualysObj = qua.Qualys(qualys_details)  # Create Qualys scanner class obj
+            qualysObj.handleAccessReq(access_details, qualys_details)  # Login | Add Asset | Add Asset Grp | Add User
+            msg +="\nQualys\nDetails send to email."
+
+        Utilities.write_to_file(msg)
+    except Exception as e:
+                Utilities.printException("In fun access_request_handler():"+ str(e))
+#---------------------Access Request Handler functions: End------------------------------------------------------------
+
+
 ''' Execution entry point: It all starts from here '''
-access_details = readAccessReq()  # Reading the Access Request
 
-try:
-    tree = parse('scanner_details.xml')  # Reading the Scanner Details
-    root = tree.getroot()
-
-    # Access request handler for scanners
-    # Read Nessus scanner details
-    scanner = root.find('nessus')
-    execute_nessus = scanner.get('enabled')
-    if execute_nessus == '1':
-        # print(scanner)
-
-        if scanner.find('host').text is None or scanner.find('username').text is None or scanner.find('host').text is None:
-            xml_error("Nessus data missing in scanner_details.xml")
-        print("Nessus" + " host@:" + scanner.find('host').text)
-        # print(scanner.find('username').text)
-        usr_passwd = input("Please enter your password for " + " Nessus" + ": ")
-        nessus_details = {'uname': scanner.find('username').text, 'passwd': usr_passwd, 'host': scanner.find('host').text}
-        # Scanner task calls from here
-        Utilities.printLog("Executing Nessus tasks")
-        nessusObj = nes.Nessus(nessus_details)  # Create Nessus scanner class obj
-        msg = nessusObj.handleAccessReq(access_details, nessus_details)  # Login | Add User | Logout
-
-    # Read Nexpose scanner details
-    scanner = root.find('nexpose')
-    execute_nexpose = scanner.get('enabled')
-    if execute_nexpose == '1':
-        # print(scanner)
-        if scanner.find('host').text is None or scanner.find('username').text is None or scanner.find('host').text is None:
-            xml_error("Nexpose data missing in scanner_details.xml")
-        print("Nexpose" + " host@:" + scanner.find('host').text)
-        # print(scanner.find('username').text)
-        usr_passwd = input("Please enter your password for " + " Nexpose" + ": ")
-        nexpose_details = {'uname': scanner.find('username').text, 'passwd': usr_passwd, 'host': scanner.find('host').text}
-        # Scanner task calls from here
-        Utilities.printLog("Executing Nexpose tasks")
-        nexposeObj = nex.Nexpose(nexpose_details)  # Create Nexpose scanner class obj
-        msg += "\n"+nexposeObj.handleAccessReq(access_details, nexpose_details)  # Login | SaveSite | Add User | Logout
-
-    # Read Qualys scanner details
-    scanner = root.find('qualys')
-    execute_qualys = scanner.get('enabled')
-    if execute_qualys == '1':
-        # print(scanner)
-        if scanner.find('host').text is None or scanner.find('username').text is None or scanner.find('host').text is None:
-            xml_error("Qualys data missing in scanner_details.xml")
-        print("Qualys" + " host@:" + scanner.find('host').text)
-        # print(scanner.find('username').text)
-        usr_passwd = input("Please enter your password for " + " Qualys" + ": ")
-        qualys_details = {'uname': scanner.find('username').text, 'passwd': usr_passwd, 'host': scanner.find('host').text}
-        # Scanner task calls from here
-        Utilities.printLog("Executing Qualys tasks")
-        qualysObj = qua.Qualys(qualys_details)  # Create Qualys scanner class obj
-        qualysObj.handleAccessReq(access_details, qualys_details)  # Login | Add Asset | Add Asset Grp | Add User
-        msg +="\nQualys\nDetails send to email."
-
-    Utilities.write_to_file(msg)
-except Exception as e:
-            Utilities.printException("Error with access_request.xml."+ str(e))
+# Command line parameter handling
+parser = argparse.ArgumentParser()
+parser.add_argument("action", help="Scanner tasks", choices=['adduser', 'scan'])
+args = parser.parse_args()
+if args.action == 'adduser':
+    access_request_handler()
+if args.action == 'scan':
+        print("Scanner Functions: TBD")
 
