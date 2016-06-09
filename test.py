2d62f39364f64e3135a447ea92338ee033b0b664
nikhilgeorge <nikhilgeorge@vmware.com>
2016-06-09 13:53:04 +0530

    Renames the main.py to vulscapi.py

--- test.py
+++ test.py
@@ -25,10 +25,38 @@
 # Utilities.printError(gen_code())
 
 
-msg = "outside\nnew\n"
+# msg = "outside\nnew\n"
+#
+# if True:
+#     msg += "insideZZZZ121212ZZ"
+#
+# f = open('User_details_toMail.txt', 'w')
+# f.writ
 
-if True:
-    msg += "insideZZZZ121212ZZ"
+#
+#
+# from StringIO import StringIO
+# from lxml import etree
+#
+# dtd = etree.DTD(StringIO("""<!ELEMENT foo EMPTY>"""))
+# root = etree.XML("<foo/>")
+# print(dtd.validate(root))
+# # True
+#
+# root = etree.XML("<foo>bar</foo>")
+# print(dtd.validate(root))
+# # False
+# print(dtd.error_log.filter_from_errors())
+# # <string>:1:0:ERROR:VALID:DTD_NOT_EMPTY: Element foo was declared EMPTY this one has content
 
-f = open('User_details_toMail.txt', 'w')
-f.write(msg)
+
+
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("action",help="Scanner tasks to perform", choices=['adduser','scan'])
+args = parser.parse_args()
+if args.action == 'adduser':
+        print("adduser")
+if args.action == 'scan':
+        print("scan")
