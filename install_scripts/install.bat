268e9e191e02eb5e92808537185552542444ab8d
akshaymeghraj <amegharaj@vmware.com>
2016-07-20 12:41:17 +0530

    adding install scripts

---
+++ install_scripts/install.bat
@@ -0,0 +1,3 @@
+python pip-8.1.2-py2.py3-none-any.whl/pip install --no-index pip-8.1.2-py2.py3-none-any.whl
+pip install requests defusedxml
+pause
---
+++ install_scripts/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo python pip-8.1.2-py2.py3-none-any.whl/pip install --no-index pip-8.1.2-py2.py3-none-any.whl
+sudo pip install setuptools requests defusedxml
